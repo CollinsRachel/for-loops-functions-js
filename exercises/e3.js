c767132ca6b9702dddee2537aaa7df368d5bec5a
Rachel M. Collins <RachelmmCollins@gmail.com>
2023-06-29 08:04:58 -0700

    changing string to num

--- exercises/e3.js
+++ exercises/e3.js
@@ -32,7 +32,7 @@ export function getStringSum(str) {
   let temp = 0; 
   for(let i =0; i <= str.length; i++) {
     if(str.charAt(i) >= '0' && str.charAt(i) <= '9') {
-      temp += str.charAt(i);
+      temp += Number(str.charAt(i));
     }
   }
   return temp;
