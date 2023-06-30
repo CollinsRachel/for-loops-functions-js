cb6d8bbd1605b01ada346ed4be28376d3a4543b7
Rachel M. Collins <RachelmmCollins@gmail.com>
2023-06-30 02:44:30 -0700

    ex 7
    create array that will be returned in the end and initiating var to keep the least positive balance, use for loop to go through array if the account balance has more than zero and is less than the current least positive balance then return the initial array

--- exercises/e7.js
+++ exercises/e7.js
@@ -6,7 +6,15 @@
 
 export function getClientWithLeastPositiveBalance(array) {
   // Your code goes here...
-  
+  let lbAccounts = [];
+  let lPosBalance = array[0].balance;
+  for (let account of array) {
+    if(account.balance != 0 && account.balance < lPosBalance) {
+      lPosBalance = account.balance;
+      lbAccounts = [account];
+    }
+  }
+  return lbAccounts;
 }
 
 // === TEST YOURSELF ===
