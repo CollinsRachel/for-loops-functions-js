a101d4ab13210585319d00e8c9f423529179b6fc
Rachel M. Collins <RachelmmCollins@gmail.com>
2023-06-30 02:03:53 -0700

    ex 5
    changing var name back to let name

--- exercises/e5.js
+++ exercises/e5.js
@@ -8,7 +8,7 @@ export function getAllClientNames(array) {
   // Your code goes here...
   const names = [];
   for (let account of array) {
-    var name = account.name;
+    let name = account.name;
     names.push(name)
   }
   return names;
